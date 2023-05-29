acbf6a11781dc252b86b356ec56fd1ba726e5b37
Varun jha <jhavarun999@gmail.com>
2023-05-29 10:34:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Navbar } from './Navbar/Navbar';
+import AllRoutes from './Routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='top'>Refer & Earn Desktop</div>
+      <Navbar />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Screen1/Referral/ReferralCode.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./referral.module.css";
+
+const ReferralCode = () => {
+  return (
+    // {/* ------ Referral code ----------- */}
+    <div className={style.ReferralCode}>
+      <h3>Your Referral Code</h3>
+      <div>
+        <h4>&emsp;EDCH54</h4>
+      </div>
+    </div>
+  );
+};
+
+export default ReferralCode;
